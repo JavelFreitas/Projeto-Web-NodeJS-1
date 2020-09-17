3c7eaa9f558e9f61f58b06f60ec0cb6dbf9b93a0
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-09-16 22:50:26 -0300

    Starting SalvarLogin functionality

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
+const saveInFile = async (email) => {
+    console.log(email);
+}
+
 app.get('', (req, res) => {
     res.status(200).send('ok')
-
 });
 
+app.get('/SalvarLogin/:login', (req, res) => {
+    const { login } = req.params;
+    saveInFile(login);
+    res.status(200).send(login);
+});
 
 app.listen(9999, () => {
     console.log('Hello World');
