435f4387a59d0dc6e4ff0ce3267c50f8dbae1e5b
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-09-17 22:53:12 -0300

    Adding validate.js to the project. Creating a validation function to get and post. implementing the function inside get and post.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const validate = require('validate.js');
 const fs = require('fs');
 
 const app = express();
@@ -7,7 +8,29 @@ const saveInFile = async (email) => {
     console.log(email);
 }
 
-app.get('', (req, res) => {
+const emailValidation = (req, res) => {
+    const { login } = req.params;
+    if (!login){
+        res.send(400).send(`message not received`);
+        return;
+    }
+
+    let validation = validate({this: `${login}`}, constraints);
+
+    console.log(login);
+    if(!validation){
+        return res.status(200).send(login);
+    }
+    return res.status(400).send(validation.this);
+}
+
+var constraints = {
+    this: {
+      email: true
+    }
+};
+
+app.get('/', (req, res) => {
     res.status(200).send('ok')
 });
 
@@ -15,6 +38,14 @@ app.get('/SalvarLogin/:login', (req, res) => {
     const { login } = req.params;
     saveInFile(login);
     res.status(200).send(login);
+});
+
+app.get('/:login', (req, res) => {
+    emailValidation(req, res);
+});
+
+app.post('/:login', (req, res) => {
+    emailValidation(req, res);
 });
 
 app.listen(9999, () => {
